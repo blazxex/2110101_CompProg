a71d0757af6029e3c8c54d55015301375e7c3dfd
Blazxex <armygm162@gmail.com>
2023-02-14 10:36:21 +0700

    05_LIST

---
+++ Part-I-04-Bowling.py
@@ -0,0 +1,13 @@
+result = input().strip()
+command = int(input())
+score = []
+
+def checkScore(rS):
+    if rS == 'x':
+        
+
+
+for i in range(len(result)):
+    if result[i]=='X':
+        s = 10
+        score.append()
