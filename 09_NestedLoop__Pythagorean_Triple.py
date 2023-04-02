b4c46fb62e0b982fd5a6afc7457995b1be9bf8f3
Blazxex <armygm162@gmail.com>
2023-04-02 23:42:54 +0700

    add new code

---
+++ 09_NestedLoop__Pythagorean_Triple.py
@@ -0,0 +1,12 @@
+def gcd(a,b):
+    while b != 0:
+        a,b = b, a%b
+    return a
+def is_coprime(a, b, c):
+    pass
+
+
+
+def primitive_Pythagorean_triples(max_len): 
+    pass
+return triple
